f047447e1cfb06159aa7bbedcca8cc1547f307e0
Jade Hernandez <jade.hernandez88@gmail.com>
2026-04-09 12:58:40 +0200

    feat(api): define Artwork type

---
+++ src/types/artwork.ts
@@ -0,0 +1,11 @@
+type Artwork = {
+  id: number;
+  title: string;
+  image_id?: string;
+  artist_display?: string;
+  date_display?: string;
+  medium_display?: string;
+  description?: string;
+};
+
+export type { Artwork };
