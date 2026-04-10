67eaf56d3d38985e23cf43ffdc7505a722322bb7
Jade Hernandez <jade.hernandez88@gmail.com>
2026-04-10 16:16:59 +0200

    feat(ui): add Skeleton component for loading placeholders with customizable styles

---
+++ src/components/ui/Skeleton.tsx
@@ -0,0 +1,15 @@
+import { cn } from "../../utils/utils";
+
+type SkeletonProps = {
+  className?: string;
+};
+
+function Skeleton({ className }: SkeletonProps) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-[#E5E5E5]", className)}
+    />
+  );
+}
+
+export { Skeleton };
